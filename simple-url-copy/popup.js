84e8c493a40e52db4ab0d073367842efc7d2cd72
Misoni <46831731+Misoni64T@users.noreply.github.com>
2019-08-10 20:30:09 +0900

    Photon Design

--- simple-url-copy/popup.js
+++ simple-url-copy/popup.js
@@ -1,90 +1,66 @@
 
 const AMAZON_HOST = "www.amazon.co.jp";
 
-let stores = {
-  "excludeQuery": false
-}
-
 const copyText = text => {
-  let copyTextArea = document.querySelector("#copy-textarea");
-  copyTextArea.textContent = text;
-  copyTextArea.select();
-  document.execCommand('copy');
+    let copyTextArea = document.querySelector("#copy-textarea");
+    copyTextArea.textContent = text;
+    copyTextArea.select();
+    document.execCommand('copy');
 }
 
 const extractAmazonUrl = rawUrl => {
-  const url = new URL(rawUrl);
-  if (url.host == AMAZON_HOST && url.pathname.match(/\/dp\/[A-Za-z0-9]+\//)) {
-    newUrl = url.origin + url.pathname.replace(/(^\S+)(\/dp\/[A-Za-z0-9]+\/)(.*)/, '$2');
-    return newUrl;
-  } else {
-    return rawUrl;
-  }
-}
-
-const excludeQuery = rawUrl => {
-  const url = new URL(rawUrl);
-  const newUrl = url.origin + url.pathname;
-  return newUrl;
+    const url = new URL(rawUrl);
+    if (url.host == AMAZON_HOST && url.pathname.match(/\/dp\/[A-Za-z0-9]+\//)) {
+        newUrl = url.origin + url.pathname.replace(/(^\S+)(\/dp\/[A-Za-z0-9]+\/)(.*)/, '$2');
+        return newUrl;
+    } else {
+        return rawUrl;
+    }
 }
 
 const showCopied = _ => {
-  let copied = document.querySelector("#copied");
-  copied.classList.remove("invisible");
-  setTimeout(_ => copied.classList.add("invisible"), 500);
+    let copied = document.querySelector("#copied");
+    copied.classList.remove("fadeout");
+    setTimeout(_ => copied.classList.add("fadeout"), 300);
 }
 
 const copyUrl = menuType => {
-  chrome.tabs.query({ active: true, currentWindow: true, lastFocusedWindow: true }, function (tabs) {
-    let url = tabs[0].url;
-    const title = tabs[0].title;
+    chrome.tabs.query({ active: true, currentWindow: true, lastFocusedWindow: true }, function (tabs) {
+        let url = tabs[0].url;
+        const title = tabs[0].title;
 
-    // Process AmazonURL
-    url = extractAmazonUrl(url);
+        // Process AmazonURL
+        url = extractAmazonUrl(url);
 
-    // Process Query 
-    if(stores.excludeQuery) {
-      url = excludeQuery(url);
-    }
-
-    let text;
-    switch (menuType) {
-      case "markdown":
-        text = `[${title}](${url})`
-        break;
-      case "backlog":
-        text = `[[${title}:${url}]]`
-        break;
-      case "simple":
-        text = `${title} ${url}`
-        break;
-      case "with-newline":
-        text = `${title}\n${url}`
-        break;
-    }
-    copyText(text);
-    showCopied();
-  })
+        let text;
+        switch (menuType) {
+            case "markdown":
+                text = `[${title}](${url})`
+                break;
+            case "backlog":
+                text = `[[${title}:${url}]]`
+                break;
+            case "simple":
+                text = `${title} ${url}`
+                break;
+        }
+        copyText(text);
+        showCopied();
+    })
 }
 
 const onInit = _ => {
-  // First copy simple
-  copyUrl("simple");
-  document.querySelectorAll(".bettercopy-menu").forEach(el => {
-    el.addEventListener("click", onClickCopyMenu);
-  });
-  document.querySelector("#switchExcludeQuery")
-          .addEventListener("click", onClickSwitchExcludeQuery);
+    // First copy simple
+    copyUrl("simple");
+    document.querySelectorAll(".bettercopy-menu").forEach(el => {
+        el.addEventListener("click", onClickCopyMenu);
+    });
 }
 
-const onClickSwitchExcludeQuery = evt => {
-  stores.excludeQuery = evt.srcElement.checked;
-  console.log(evt.srcElement.checked);
-}
 
-const onClickCopyMenu = function(evt){
-  const menuType = this.id;
-  copyUrl(menuType);
+const onClickCopyMenu = function (evt) {
+    const menuType = this.id;
+    copyUrl(menuType);
 }
 
 document.addEventListener("DOMContentLoaded", onInit);
