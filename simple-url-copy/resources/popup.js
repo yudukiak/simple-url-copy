896e9a9cdf28eabfd4e1fcad470d7e7a179fef7e
micelle <micelle_yamame@hotmail.co.jp>
2019-12-09 13:11:57 +0900

    [update] /gp/product/にも対応

--- simple-url-copy/resources/popup.js
+++ simple-url-copy/resources/popup.js
@@ -7,9 +7,16 @@ const copyText = text => {
 
 const extractAmazonUrl = rawUrl => {
   const url = new URL(rawUrl);
-  if (url.host == AMAZON_HOST && url.pathname.match(/\/dp\/[A-Za-z0-9]/)) {
-    newUrl = url.origin + url.pathname.replace(/(^\S+)(\/dp\/[A-Za-z0-9]{10})(.*)/, '$2');
-    return newUrl;
+  if (url.host == AMAZON_HOST) {
+    if (url.pathname.match(/\/dp\/[A-Za-z0-9]/)) {
+      newUrl = url.origin + url.pathname.replace(/(^\S+)(\/dp\/[A-Za-z0-9]{10})(.*)/, '$2');
+      return newUrl;
+    } else if (url.pathname.match(/\/gp\/product\/[A-Za-z0-9]/)) {
+      newUrl = url.origin + url.pathname.replace(/(^\S+)?(\/gp\/product\/[A-Za-z0-9]{10})(.*)/, '$2');
+      return newUrl;
+    } else {
+      return rawUrl;
+    }
   } else {
     return rawUrl;
   }
