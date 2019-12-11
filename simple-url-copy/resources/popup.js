da0b737ceec887eb39642c1d0870cd2974a2b258
micelle <micelle_yamame@hotmail.co.jp>
2019-12-11 15:19:24 +0900

    [update] 見ないけど/gp/aw/d/にも対応

--- simple-url-copy/resources/popup.js
+++ simple-url-copy/resources/popup.js
@@ -14,6 +14,9 @@ const extractAmazonUrl = rawUrl => {
     } else if (url.pathname.match(/\/gp\/product\/[A-Za-z0-9]/)) {
       newUrl = url.origin + url.pathname.replace(/(^\S+)?(\/gp\/product\/[A-Za-z0-9]{10})(.*)/, '$2');
       return newUrl;
+    } else if (url.pathname.match(/\/gp\/aw\/d\/[A-Za-z0-9]/)) {
+      newUrl = url.origin + url.pathname.replace(/(^\S+)?(\/gp\/aw\/d\/[A-Za-z0-9]{10})(.*)/, '$2');
+      return newUrl;
     } else {
       return rawUrl;
     }
